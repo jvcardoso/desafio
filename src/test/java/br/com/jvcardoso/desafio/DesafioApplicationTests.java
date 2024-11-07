856e48bbae0ea37d7c23af07544c9ee36e647d58
Juliano Ventura Cardoso <juliano.cardoso@dasa.com.br>
2024-11-06 21:12:21 -0300

    Carga inicial 2

---
+++ src/test/java/br/com/jvcardoso/desafio/DesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jvcardoso.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
